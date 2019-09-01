a40fa41d974565c60f6caa41c08205774af33c77
Makaronov <kokiys@yandex.ru>
2019-09-01 20:59:24 +0200

    Cache usage added

---
+++ app/src/main/java/com/makaroni/usersphoto/cache/FileCache.java
@@ -0,0 +1,39 @@
+package com.makaroni.usersphoto.cache;
+
+import java.io.File;
+import android.content.Context;
+
+public class FileCache {
+    
+    private File cacheDir;
+    boolean t ;
+    
+    public FileCache(Context context){
+        //Find the dir to save cached images
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED))
+            cacheDir=new File(android.os.Environment.getExternalStorageDirectory(),"UsersPhoto");
+        else
+            cacheDir=context.getCacheDir();
+        if(!cacheDir.exists())
+            t = cacheDir.mkdirs();
+    }
+    
+    public File getFile(String url){
+        //I identify images by hashcode. Not a perfect solution, good for the demo.
+        String filename=String.valueOf(url.hashCode());
+        //Another possible solution (thanks to grantland)
+        //String filename = URLEncoder.encode(url);
+        File f = new File(cacheDir, filename);
+        return f;
+        
+    }
+    
+    public void clear(){
+        File[] files=cacheDir.listFiles();
+        if(files==null)
+            return;
+        for(File f:files)
+            f.delete();
+    }
+
+}
